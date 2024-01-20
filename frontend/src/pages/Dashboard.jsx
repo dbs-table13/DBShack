673dae2eb03b5009bd1dc3161a130ada249656a0
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2024-01-20 10:39:55 +0800

    add material

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
 const Dashboard = () => {
 	return <div>Dashboard</div>;
 };
