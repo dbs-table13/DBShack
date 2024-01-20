b44f9305cbf6071e7fc937e3daa4ecdc11fab82e
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2024-01-20 16:13:48 +0800

    make bigger add button

--- frontend/src/components/Itinerary.jsx
+++ frontend/src/components/Itinerary.jsx
@@ -1,121 +1,149 @@
-
-import '../App.css'
-import EditRoundedIcon from '@mui/icons-material/EditRounded';
-import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions } from '@mui/material';import Grid from '@mui/material/Grid';
-import Popup from './Popup';
+import "../App.css";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Popup from "./Popup";
 import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
 
-import axios from 'axios';
-import IconButton from '@mui/material/IconButton';
-
+import axios from "axios";
+import IconButton from "@mui/material/IconButton";
 
 function ItineraryList() {
+  const [open, setOpen] = useState(false);
+  // const [itineraryDetailsModal,setItineraryDetailsModal] = useState(false);
+  // const [itineraryList,setItineraryList] = useState([])
+  const itineraryList = [
+    {
+      itinerary_id: 1,
+      budget: 300,
+      title: "Austria Itinerary1",
+      destinations: [
+        { id: 1, name: "Marina Bay Sands", cost: 50, notes: "Iconic hotel" },
+        { id: 2, name: "Sentosa Island", cost: 50, notes: "Futuristic hotel" },
+      ],
+    },
+    {
+      itinerary_id: 2,
+      budget: 300,
+      title: "Austria Itinerary2",
+      destinations: [
+        { id: 1, name: "Marina Bay Sands", cost: 50, notes: "Iconic hotel" },
+        { id: 2, name: "Sentosa Island", cost: 50, notes: "Futuristic hotel" },
+      ],
+    },
+  ];
 
-	const [open,setOpen] = useState(false);
-	// const [itineraryDetailsModal,setItineraryDetailsModal] = useState(false);
-	// const [itineraryList,setItineraryList] = useState([])
-	const itineraryList = [
-		{itinerary_id: 1,budget: 300, title: 'Austria Itinerary1',destinations:[{id:1,name:"Marina Bay Sands",cost:50,notes:"Iconic hotel"},{id:2,name:"Sentosa Island",cost:50,notes:"Futuristic hotel"}]},
-		{itinerary_id: 2,budget: 300, title: 'Austria Itinerary2',destinations:[{id:1,name:"Marina Bay Sands",cost:50,notes:"Iconic hotel"},{id:2,name:"Sentosa Island",cost:50,notes:"Futuristic hotel"}]},
+  // axios.get(`https://localhost:3001/getItinerary`)
+  // 	.then(res => {
+  // 		setItineraryList(res.data;
+  // 	})
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-	];	
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
 
-	// axios.get(`https://localhost:3001/getItinerary`)
-	// 	.then(res => {
-	// 		setItineraryList(res.data;
-	// 	})
-		
-	const handleOpen = () => {
-		setOpen(true);
-	};
+  const handleDeleteClick = (itineraryID) => {
+    // Ask for confirmation
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this itinerary?",
+    );
 
+    // If the user confirms, proceed with the delete request
+    if (isConfirmed) {
+      deleteItinerary(itineraryID);
+    }
+  };
 
-		
-	const Item = styled(Paper)(({ theme }) => ({
-		backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-		...theme.typography.body2,
-		padding: theme.spacing(1),
-		textAlign: 'center',
-		color: theme.palette.text.secondary,
-	}));
-		
+  const deleteItinerary = async (itineraryID) => {
+    try {
+      console.log(itineraryID);
+      alert("https://localhost:3001/itinerary/" + itineraryID);
+      const response = await axios.post(
+        "https://localhost:3001/itinerary/" + itineraryID,
+      );
+      alert("deleted", response.data);
+    } catch (error) {
+      alert("Error deleting post:", error);
+    }
+  };
 
-	const handleDeleteClick = (itineraryID) => {
-		// Ask for confirmation
-		const isConfirmed = window.confirm('Are you sure you want to delete this itinerary?');
-	
-		// If the user confirms, proceed with the delete request
-		if (isConfirmed) {
-			deleteItinerary(itineraryID);
-		}
-	}
-	
-	const deleteItinerary=async(itineraryID)=>{
-
-		try {
-			console.log(itineraryID)
-			alert("https://localhost:3001/itinerary/"+itineraryID )
-			const response = await axios.post("https://localhost:3001/itinerary/"+itineraryID);
-			alert("deleted", response.data);
-		} catch (error) {
-			alert("Error deleting post:", error);
-		}
-	}
-
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={2}>
-			{itineraryList.map(item => (
-				<Grid item xs={12} md={6} lg={4} key={item.itinerary_id}>
-				<Item sx={{ padding: 0 }}>
-					<Card>
-						<CardActionArea>
-							<CardMedia
-							component="img"
-							height="200"
-							image="./src/static/images/pexels-pixabay-461755.jpg"
-							alt="green iguana"
-							/>
-							<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-								{item.title}
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
-								{item.budget}
-							</Typography>
-							</CardContent>
-						</CardActionArea>
-						<IconButton onClick={() => {handleOpen}}>
-							<EditRoundedIcon></EditRoundedIcon>
-						</IconButton>
-						<IconButton onClick={() => { handleDeleteClick(item.itinerary_id) }}>
-							<DeleteForeverRoundedIcon></DeleteForeverRoundedIcon>
-						</IconButton>
-						<Popup action="update" data={item} open={open} setOpen={setOpen}>
-						</Popup>					
-						</Card>
-				</Item>
-				</Grid>
-			))}
-			    <Grid item xs={12} md={6} lg={4}>
-					<IconButton onClick={handleOpen}>
-						<AddCircleOutlineRoundedIcon />
-					</IconButton>
-				</Grid>
-			</Grid>	  	
-		</Box>
-	);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {itineraryList.map((item) => (
+          <Grid item xs={12} md={6} lg={4} key={item.itinerary_id}>
+            <Item sx={{ padding: 0 }}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="./src/static/images/pexels-pixabay-461755.jpg"
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.budget}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <IconButton
+                  onClick={() => {
+                    handleOpen;
+                  }}
+                >
+                  <EditRoundedIcon></EditRoundedIcon>
+                </IconButton>
+                <IconButton
+                  onClick={() => {
+                    handleDeleteClick(item.itinerary_id);
+                  }}
+                >
+                  <DeleteForeverRoundedIcon></DeleteForeverRoundedIcon>
+                </IconButton>
+                <Popup
+                  action="update"
+                  data={item}
+                  open={open}
+                  setOpen={setOpen}
+                ></Popup>
+              </Card>
+            </Item>
+          </Grid>
+        ))}
+        <Grid item xs={12} md={6} lg={4}>
+          <IconButton onClick={handleOpen}>
+            <Fab color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
-		
+
 export default ItineraryList;
