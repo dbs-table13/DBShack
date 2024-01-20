6faa70e272f2f6cdffcc01d40b2a9dd9ba5f23c3
LaLaNiceApp <eunjin.kim.2020@sis.smu.edu.sg>
2024-01-20 11:01:15 +0800

    added card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,7 +7,30 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	return <div>
+		<h1>Dashboard</h1>
+		<Box sx={{ maxWidth: 345 }}>
+			<Card>
+				<CardMedia
+					component="img"
+					height="140"
+					image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+					alt="green iguana"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						Lizard
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						Lizards are a widespread group of squamate reptiles, with over 6,000
+						species, ranging across all continents except Antarctica
+					</Typography>
+				</CardContent>
+				<Button variant="contained">Edit</Button>
+				<Button variant="contained">Delete</Button>
+			</Card>
+		</Box>
+	</div>;
 };
 
 export default Dashboard;
